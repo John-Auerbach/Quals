5e8dbd8736168f8c4794d53aba45f14c425f2efc
John Auerbach <johnauerbach01@gmail.com>
2024-12-26 21:10:12 -0800

    deployment stuff, probably a mistake

--- app.py
+++ app.py
@@ -149,4 +149,6 @@ def update_progress():
     return jsonify({"status": "success", "newProgress": progress[subject][topic]["overall"]})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
